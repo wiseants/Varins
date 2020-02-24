3bddf9c55d97809abdb96ec29c47c5434817190d
hcan <wiseants@nate.com>
2020-02-24 11:17:29 +0900

    Remove usings.

--- HVision.Common/Tool/UnityNameTool.cs
+++ HVision.Common/Tool/UnityNameTool.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace HVision.Common.Tool
 {
-	public class UnityNameTool
+    public class UnityNameTool
 	{
 		public static string ConvertViewName(string name)
 		{
